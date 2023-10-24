6446ed7416059457da66dad97e1a313eb6ae3c57
matheusbp <matheusbpamplona@outlook.com>
2023-10-24 19:55:21 -0300

    ajeitando o workflow1

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "bucket_more_green"  # Nome do bucket S3 onde o estado do Terraform será armazenado
+    bucket         = "bucket-more-green"  # Nome do bucket S3 onde o estado do Terraform será armazenado
     key            = "terraform.tfstate"  # Nome do arquivo no bucket que conterá o estado do Terraform
     region         = "us-east-1" # Região da AWS onde o bucket S3 está localizado
     encrypt        = true # Habilitar a criptografia do estado do Terraform
